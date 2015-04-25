c7add732ba58a9c1ab65a16a4b21c55372602d3d
陈天昱 <chentianyu@outlook.com>
2015-04-25 16:34:32 +0800

    finish lab4 code

--- labcodes/lab4/kern/mm/pmm.c
+++ labcodes/lab4/kern/mm/pmm.c
@@ -363,7 +363,7 @@ pmm_init(void) {
 // return vaule: the kernel virtual address of this pte
 pte_t *
 get_pte(pde_t *pgdir, uintptr_t la, bool create) {
-    /* LAB2 EXERCISE 2: YOUR CODE
+    /* LAB2 EXERCISE 2: 2012011348
      *
      * If you need to visit a physical address, please use KADDR()
      * please read pmm.h for useful macros
@@ -396,6 +396,30 @@ get_pte(pde_t *pgdir, uintptr_t la, bool create) {
     }
     return NULL;          // (8) return page table entry
 #endif
+
+    // find page directory entry
+    pde_t * pdep = & pgdir[PDX(la)];
+    // if page dir entry does not exist
+    if((* pdep & PTE_P) == 0) {
+        // if creation is needed
+        if(create) {
+            struct Page * page = alloc_page();
+            // just in case
+            if(page == NULL)
+                return NULL;
+            // set reference
+            set_page_ref(page, 1);
+            // clear page content
+            uintptr_t pa = page2pa(page);
+            memset(KADDR(pa), 0, PGSIZE);
+            * pdep = pa | PTE_U | PTE_W | PTE_P;
+        } else {
+            return NULL;
+        }
+    } 
+
+    // return 
+    return & ((pte_t *) KADDR(PDE_ADDR(* pdep)))[PTX(la)];
 }
 
 //get_page - get related Page struct for linear address la using PDT pgdir
@@ -416,7 +440,7 @@ get_page(pde_t *pgdir, uintptr_t la, pte_t **ptep_store) {
 //note: PT is changed, so the TLB need to be invalidate 
 static inline void
 page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
-    /* LAB2 EXERCISE 3: YOUR CODE
+    /* LAB2 EXERCISE 3: 2012011348
      *
      * Please check if ptep is valid, and tlb must be manually updated if mapping is updated
      *
@@ -441,6 +465,18 @@ page_remove_pte(pde_t *pgdir, uintptr_t la, pte_t *ptep) {
                                   //(6) flush tlb
     }
 #endif
+    // check if pte is present
+    if((* ptep) & PTE_P) {
+        struct Page * page = pte2page(* ptep);
+        // decrease page reference 
+        // free page when reference reaches 0
+        if(page_ref_dec(page) == 0) {
+            free_page(page);
+        }
+        * ptep = 0;
+        // !! flush TLB !!
+        tlb_invalidate(pgdir, la);
+    }
 }
 
 //page_remove - free an Page which is related linear address la and has an validated pte
--- labcodes/lab4/kern/process/proc.c
+++ labcodes/lab4/kern/process/proc.c
@@ -86,7 +86,7 @@ static struct proc_struct *
 alloc_proc(void) {
     struct proc_struct *proc = kmalloc(sizeof(struct proc_struct));
     if (proc != NULL) {
-    //LAB4:EXERCISE1 YOUR CODE
+    //LAB4:EXERCISE1 2012011348
     /*
      * below fields in proc_struct need to be initialized
      *       enum proc_state state;                      // Process state
@@ -102,8 +102,21 @@ alloc_proc(void) {
      *       uint32_t flags;                             // Process flag
      *       char name[PROC_NAME_LEN + 1];               // Process name
      */
-    }
-    return proc;
+    proc -> state = PROC_UNINIT;
+    proc -> pid = -1;
+    proc -> runs = 0;
+    proc -> kstack = 0;
+    proc -> need_resched = 0;
+    proc -> parent = NULL;
+    proc -> mm = NULL;
+    memset(& (proc -> context), 0, sizeof(struct context));
+    proc -> tf = NULL;
+    proc -> cr3 = boot_cr3;
+    proc -> flags = 0;
+    memset(proc -> name, 0, PROC_NAME_LEN);
+  }
+
+  return proc;
 }
 
 // set_proc_name - set the name of proc
@@ -296,6 +309,40 @@ do_fork(uint32_t clone_flags, uintptr_t stack, struct trapframe *tf) {
     //    5. insert proc_struct into hash_list && proc_list
     //    6. call wakup_proc to make the new child process RUNNABLE
     //    7. set ret vaule using child proc's pid
+    // 1
+    proc = alloc_proc();
+    if(proc == NULL) {
+      goto fork_out;
+    }
+
+    proc -> parent = current;
+
+    // 2
+    if(setup_kstack(proc) != 0) {
+      goto bad_fork_cleanup_proc;
+    }
+    // 3
+    if(copy_mm(clone_flags, proc) != 0) {
+      goto bad_fork_cleanup_kstack;
+    }
+    // 4
+    copy_thread(proc, stack, tf);
+
+    // 5
+    bool intr_flag;
+    local_intr_save(intr_flag);
+    {
+      proc -> pid = get_pid();
+      hash_proc(proc);
+      list_add(& proc_list, & (proc -> list_link));
+      nr_process ++;
+    }
+    local_intr_restore(intr_flag);
+    // 6
+    wakeup_proc(proc);
+    // 7
+    ret = proc -> pid;
+
 fork_out:
     return ret;
 
